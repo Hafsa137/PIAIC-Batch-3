da5920d2bd8ce2b348dc5573d8c1db72d0673df9
Hafsa Arif <hafsa.arif11@gmail.com>
2019-12-14 12:40:27 +0500

    tasks given in class
    print value 1000 to 990 with (while and for loop)

---
+++ task1.py
@@ -0,0 +1,10 @@
+print("\nwith for loop \n")
+for i in range(0, 11):
+    print(1000 - i)
+
+print("\nwith while loop \n")
+counter = 1000  # Counter
+
+while counter >= 990:  # Logic
+    print(counter)
+    counter -= 1  # Increment
