f2a9c1b1c821b58ba7bdb36ec2bf424487de332b
Hafsa Arif <hafsa.arif11@gmail.com>
2019-12-14 12:11:15 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,6 @@
+username = input('username')
+password = input('password')
+if (username == "hafsa.arif11@gmail.com" or username == "03214567890" or username == "25698-7415632-4") and password == "123":
+    print("valid")
+else:
+    print("not valid")
